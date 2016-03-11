ac81e7f645a6a831e54266d0064bac590648a022
Piotr Wittchen <piotr@wittchen.biz.pl>
2016-03-11 15:45:03 +0100

    code refactoring and removing comments

--- app/src/main/java/pl/polidea/gesturedetector/BetterGestureDetectorActivity.java
+++ app/src/main/java/pl/polidea/gesturedetector/BetterGestureDetectorActivity.java
@@ -10,14 +10,13 @@ import pl.polidea.test.R;
 
 public class BetterGestureDetectorActivity extends Activity implements BetterGestureListener {
     private BetterGestureDetector gestureDetector;
-    private TextView v;
+    private TextView textView;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        v = (TextView) findViewById(R.id.textView1);
+        textView = (TextView) findViewById(R.id.textView);
         gestureDetector = new BetterGestureDetector(this);
     }
 
@@ -29,50 +28,50 @@ public class BetterGestureDetectorActivity extends Activity implements BetterGes
 
     @Override
     public void onPress(MotionEvent motionEvent) {
-        v.setText("press");
-        v.setBackgroundColor(0xffff0000);
-        v.invalidate();
+        textView.setText("press");
+        textView.setBackgroundColor(0xffff0000);
+        textView.invalidate();
     }
 
     @Override
     public void onTap(MotionEvent motionEvent) {
-        v.setText("tap");
-        v.setBackgroundColor(0xff00ff00);
-        v.invalidate();
+        textView.setText("tap");
+        textView.setBackgroundColor(0xff00ff00);
+        textView.invalidate();
     }
 
     @Override
     public void onDrag(MotionEvent motionEvent) {
-        v.setText("drag");
-        v.setBackgroundColor(0xff0000ff);
-        v.invalidate();
+        textView.setText("drag");
+        textView.setBackgroundColor(0xff0000ff);
+        textView.invalidate();
     }
 
     @Override
     public void onMove(MotionEvent motionEvent) {
-        v.setText("move");
-        v.setBackgroundColor(0xffff00ff);
-        v.invalidate();
+        textView.setText("move");
+        textView.setBackgroundColor(0xffff00ff);
+        textView.invalidate();
     }
 
     @Override
     public void onRelease(MotionEvent motionEvent) {
-        v.setText("release");
-        v.setBackgroundColor(0xffffff00);
-        v.invalidate();
+        textView.setText("release");
+        textView.setBackgroundColor(0xffffff00);
+        textView.invalidate();
     }
 
     @Override
     public void onLongPress(MotionEvent motionEvent) {
-        v.setText("longpress");
-        v.setBackgroundColor(0xff00ffff);
-        v.invalidate();
+        textView.setText("longpress");
+        textView.setBackgroundColor(0xff00ffff);
+        textView.invalidate();
     }
 
     @Override
     public void onMultiTap(MotionEvent motionEvent, int clicks) {
-        v.setText("multitap [" + clicks + "]");
-        v.setBackgroundColor(0xff7f7f7f);
-        v.invalidate();
+        textView.setText("multitap [" + clicks + "]");
+        textView.setBackgroundColor(0xff7f7f7f);
+        textView.invalidate();
     }
 }
--- library/src/main/java/pl/polidea/gesturedetector/library/BetterGestureDetector.java
+++ library/src/main/java/pl/polidea/gesturedetector/library/BetterGestureDetector.java
@@ -1,338 +1,208 @@
 package pl.polidea.gesturedetector.library;
 
 import android.os.Handler;
-import android.util.Log;
 import android.view.MotionEvent;
 
-// TODO: Auto-generated Javadoc
-
-/**
- * The Class BetterGestureDetector.
- */
 public class BetterGestureDetector {
-    /** The Constant DEFAULT_PRESS_TIMEOUT. */
-    private static final int DEFAULT_PRESS_TIMEOUT = 100;
+  private static final int DEFAULT_PRESS_TIMEOUT = 100;
+  private static final int DEFAULT_TAP_TIMEOUT = 300;
+  private static final int DEFAULT_MOVE_EPSILON = 4;
+  private static final long DEFAULT_LONG_PRESS_TIMEOUT = 500;
 
-    /** The Constant DEFAULT_TAP_TIMEOUT. */
-    private static final int DEFAULT_TAP_TIMEOUT = 300;
+  private int pressTimeout = DEFAULT_PRESS_TIMEOUT;
+  private int tapTimeout = DEFAULT_TAP_TIMEOUT;
+  private int moveEpsilon = DEFAULT_MOVE_EPSILON;
+  private long longPressTimeout = DEFAULT_LONG_PRESS_TIMEOUT;
 
-    /** The Constant DEFAULT_MOVE_EPSILON. */
-    private static final int DEFAULT_MOVE_EPSILON = 4;
+  private Runnable tapHandler;
+  private Runnable pressHandler;
+  private Runnable longPressHandler;
 
-    /** The press timeout. */
-    private int pressTimeout = DEFAULT_PRESS_TIMEOUT;
+  private long prevTouchTime;
+  private float prevTouchY;
+  private float prevTouchX;
 
-    /** The tap timeout. */
-    private int tapTimeout = DEFAULT_TAP_TIMEOUT;
+  private boolean dragging;
+  private boolean moving;
+  private int clicks = 0;
 
-    /** The move epsilon. */
-    private int moveEpsilon = DEFAULT_MOVE_EPSILON;
+  private Handler handler;
+  private final BetterGestureListener listener;
 
-    /** The handler. */
-    private Handler handler;
-
-    /** The tap handler. */
-    Runnable tapHandler;
-
-    /** The press handler. */
-    Runnable pressHandler;
-
-    /** The listener. */
-    private final BetterGestureListener listener;
-
-    /** The prev touch time. */
-    private long prevTouchTime;
-
-    /** The prev touch y. */
-    private float prevTouchY;
-
-    /** The prev touch x. */
-    private float prevTouchX;
-
-    /** The vertical move cancel. */
-    boolean verticalMoveCancel = true;
-
-    /** The horizontal move cancel. */
-    boolean horizontalMoveCancel = true;
-    /** The dragging. */
-    private boolean dragging;
-
-    /** The moving. */
-    private boolean moving;
-
-    /** The long press handler. */
-    private Runnable longPressHandler;
-
-    /** The long press timeout. */
-    private long longPressTimeout = 500;
-
-    private int clicks = 0;
-
-    /**
-     * Instantiates a new better gesture detector.
-     * 
-     * @param listener
-     *            the listener
-     */
-    public BetterGestureDetector(BetterGestureListener listener) {
-        if (listener == null) {
-            throw new NullPointerException("Listener cannot be null");
-        }
-        this.listener = listener;
-        handler = new Handler();
+  public BetterGestureDetector(BetterGestureListener listener) {
+    if (listener == null) {
+      throw new NullPointerException("listener == null");
     }
 
-    /**
-     * On touch event.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    public void onTouchEvent(final MotionEvent motionEvent) {
-        float dx = motionEvent.getX() - prevTouchX;
-        float dy = motionEvent.getY() - prevTouchY;
-        long dt = System.currentTimeMillis() - prevTouchTime;
+    this.listener = listener;
+    this.handler = new Handler();
+  }
+
+  public void onTouchEvent(final MotionEvent motionEvent) {
+    float dx = motionEvent.getX() - prevTouchX;
+    float dy = motionEvent.getY() - prevTouchY;
+    long dt = System.currentTimeMillis() - prevTouchTime;
+    handler.removeCallbacks(longPressHandler);
+    longPressHandler = null;
+
+    switch (motionEvent.getAction()) {
+      case MotionEvent.ACTION_DOWN:
+        onActionDown(motionEvent, dt);
+        break;
+      case MotionEvent.ACTION_UP:
+        onActionUp(motionEvent, dt);
+        break;
+      case MotionEvent.ACTION_MOVE:
+        onActionMove(motionEvent, dx, dy);
+        break;
+      case MotionEvent.ACTION_CANCEL:
+        onActionCancel(motionEvent);
+        break;
+    }
+
+    prevTouchX = motionEvent.getX();
+    prevTouchY = motionEvent.getY();
+  }
+
+  private void onActionCancel(MotionEvent motionEvent) {
+    handler.removeCallbacks(pressHandler);
+    pressHandler = null;
+    handler.removeCallbacks(tapHandler);
+    tapHandler = null;
+    handler.removeCallbacks(longPressHandler);
+    longPressHandler = null;
+    onRelease(motionEvent);
+  }
+
+  private void onActionMove(MotionEvent motionEvent, float dx, float dy) {
+    if (Math.abs(dx) > moveEpsilon || Math.abs(dy) > moveEpsilon || dragging || moving) {
+      if (pressHandler == null && !moving) {
+        dragging = true;
+      }
+      handler.removeCallbacks(tapHandler);
+      tapHandler = null;
+      handler.removeCallbacks(pressHandler);
+      pressHandler = null;
+      handler.removeCallbacks(longPressHandler);
+      longPressHandler = null;
+      moving = true;
+      if (dragging) {
+        onDrag(motionEvent);
+      } else {
+        onMove(motionEvent);
+      }
+    }
+  }
+
+  private void onActionUp(final MotionEvent motionEvent, long dt) {
+    if (dt > tapTimeout || moving || dragging) {
+      onRelease(motionEvent);
+      return;
+    }
+
+    tapHandler = new Runnable() {
+
+      @Override public void run() {
         handler.removeCallbacks(longPressHandler);
         longPressHandler = null;
+        onTap(motionEvent, clicks);
+      }
+    };
+    handler.postDelayed(tapHandler, tapTimeout - dt);
+    prevTouchTime += dt;
+  }
 
-        switch (motionEvent.getAction()) {
-        case MotionEvent.ACTION_DOWN:
-            dragging = false;
-            moving = false;
-            if (tapHandler != null) {
-                clicks++;
-                Log.d("clicks", "" + clicks);
-                handler.removeCallbacks(tapHandler);
-                tapHandler = null;
-            }
-
-            handler.removeCallbacks(pressHandler);
-            pressHandler = new Runnable() {
-
-                @Override
-                public void run() {
-                    // handler.removeCallbacks(tapHandler);
-                    // tapHandler = null;
-                    onPress(motionEvent);
-                }
-
-            };
-            handler.postDelayed(pressHandler, pressTimeout);
-
-            longPressHandler = new Runnable() {
-
-                @Override
-                public void run() {
-                    onLongPress(motionEvent);
-                }
-            };
-            handler.postDelayed(longPressHandler, getLongPressTimeout());
-            prevTouchTime += dt;
-            break;
-        case MotionEvent.ACTION_UP:
-            if (dt > tapTimeout || moving || dragging) {
-                onRelease(motionEvent);
-                break;
-            }
-
-            tapHandler = new Runnable() {
-
-                @Override
-                public void run() {
-                    handler.removeCallbacks(longPressHandler);
-                    longPressHandler = null;
-                    onTap(motionEvent, clicks);
-                }
-            };
-            handler.postDelayed(tapHandler, tapTimeout - dt);
-
-            prevTouchTime += dt;
-            break;
-
-        case MotionEvent.ACTION_MOVE:
-            if (Math.abs(dx) > moveEpsilon || Math.abs(dy) > moveEpsilon || dragging || moving) {
-                if (pressHandler == null && !moving) {
-                    dragging = true;
-                }
-                handler.removeCallbacks(tapHandler);
-                tapHandler = null;
-                handler.removeCallbacks(pressHandler);
-                pressHandler = null;
-                handler.removeCallbacks(longPressHandler);
-                longPressHandler = null;
-                moving = true;
-                if (dragging) {
-                    onDrag(motionEvent);
-                } else {
-                    onMove(motionEvent);
-                }
-            }
-            break;
-        case MotionEvent.ACTION_CANCEL:
-            handler.removeCallbacks(pressHandler);
-            pressHandler = null;
-            handler.removeCallbacks(tapHandler);
-            tapHandler = null;
-            handler.removeCallbacks(longPressHandler);
-            longPressHandler = null;
-            onRelease(motionEvent);
-        }
-        prevTouchX = motionEvent.getX();
-        prevTouchY = motionEvent.getY();
+  private void onActionDown(final MotionEvent motionEvent, long dt) {
+    dragging = false;
+    moving = false;
+    if (tapHandler != null) {
+      clicks++;
+      handler.removeCallbacks(tapHandler);
+      tapHandler = null;
     }
 
-    /**
-     * On release.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    private void onRelease(MotionEvent motionEvent) {
-        Log.d("gesture", "release");
-        clicks = 0;
-        listener.onRelease(motionEvent);
-    }
+    handler.removeCallbacks(pressHandler);
+    pressHandler = new Runnable() {
+      @Override public void run() {
+        onPress(motionEvent);
+      }
+    };
+    handler.postDelayed(pressHandler, pressTimeout);
 
-    /**
-     * On drag.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    private void onDrag(MotionEvent motionEvent) {
-        Log.d("gesture", "drag");
-        clicks = 0;
-        listener.onDrag(motionEvent);
-    }
+    longPressHandler = new Runnable() {
+      @Override public void run() {
+        onLongPress(motionEvent);
+      }
+    };
+    handler.postDelayed(longPressHandler, longPressTimeout);
+    prevTouchTime += dt;
+  }
 
-    /**
-     * On move.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    private void onMove(MotionEvent motionEvent) {
-        Log.d("gesture", "move");
-        clicks = 0;
-        listener.onMove(motionEvent);
-    }
+  private void onRelease(MotionEvent motionEvent) {
+    clicks = 0;
+    listener.onRelease(motionEvent);
+  }
 
-    /**
-     * On single tap up.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    protected void onTap(MotionEvent motionEvent, int clicks) {
-        tapHandler = null;
-        if (clicks == 0) {
-            Log.d("gesture", "tap");
-            listener.onTap(motionEvent);
-        } else {
-            Log.d("gesture", "multitap");
-            listener.onMultiTap(motionEvent, clicks + 1);
-        }
-        this.clicks = 0;
-    }
+  private void onDrag(MotionEvent motionEvent) {
+    clicks = 0;
+    listener.onDrag(motionEvent);
+  }
 
-    /**
-     * On long press.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    protected void onLongPress(MotionEvent motionEvent) {
-        Log.d("gesture", "longpress");
-        clicks = 0;
-        longPressHandler = null;
-        listener.onLongPress(motionEvent);
-    }
+  private void onMove(MotionEvent motionEvent) {
+    clicks = 0;
+    listener.onMove(motionEvent);
+  }
 
-    /**
-     * On show press.
-     * 
-     * @param motionEvent
-     *            the motion event
-     */
-    protected void onPress(MotionEvent motionEvent) {
-        Log.d("gesture", "press");
-        pressHandler = null;
-        listener.onPress(motionEvent);
+  protected void onTap(MotionEvent motionEvent, int clicks) {
+    tapHandler = null;
+    if (clicks == 0) {
+      listener.onTap(motionEvent);
+    } else {
+      listener.onMultiTap(motionEvent, clicks + 1);
     }
+    this.clicks = 0;
+  }
 
-    /**
-     * Gets the press timeout.
-     * 
-     * @return the press timeout
-     */
-    public int getPressTimeout() {
-        return pressTimeout;
-    }
+  private void onLongPress(MotionEvent motionEvent) {
+    clicks = 0;
+    longPressHandler = null;
+    listener.onLongPress(motionEvent);
+  }
 
-    /**
-     * Sets the press timeout.
-     * 
-     * @param pressTimeout
-     *            the new press timeout
-     */
-    public void setPressTimeout(int pressTimeout) {
-        this.pressTimeout = pressTimeout;
-    }
+  private void onPress(MotionEvent motionEvent) {
+    pressHandler = null;
+    listener.onPress(motionEvent);
+  }
 
-    /**
-     * Gets the tap timeout.
-     * 
-     * @return the tap timeout
-     */
-    public int getTapTimeout() {
-        return tapTimeout;
-    }
+  public int getPressTimeout() {
+    return pressTimeout;
+  }
 
-    /**
-     * Sets the tap timeout.
-     * 
-     * @param tapTimeout
-     *            the new tap timeout
-     */
-    public void setTapTimeout(int tapTimeout) {
-        this.tapTimeout = tapTimeout;
-    }
+  public void setPressTimeout(int pressTimeout) {
+    this.pressTimeout = pressTimeout;
+  }
 
-    /**
-     * Gets the move epsilon.
-     * 
-     * @return the move epsilon
-     */
-    public int getMoveEpsilon() {
-        return moveEpsilon;
-    }
+  public int getTapTimeout() {
+    return tapTimeout;
+  }
 
-    /**
-     * Sets the move epsilon.
-     * 
-     * @param moveEpsilon
-     *            the new move epsilon
-     */
-    public void setMoveEpsilon(int moveEpsilon) {
-        this.moveEpsilon = moveEpsilon;
-    }
+  public void setTapTimeout(int tapTimeout) {
+    this.tapTimeout = tapTimeout;
+  }
 
-    /**
-     * Gets the long press timeout.
-     * 
-     * @return the long press timeout
-     */
-    public long getLongPressTimeout() {
-        return longPressTimeout;
-    }
+  public int getMoveEpsilon() {
+    return moveEpsilon;
+  }
 
-    /**
-     * Sets the long press timeout.
-     * 
-     * @param longPressTimeout
-     *            the new long press timeout
-     */
-    public void setLongPressTimeout(long longPressTimeout) {
-        this.longPressTimeout = longPressTimeout;
-    }
+  public void setMoveEpsilon(int moveEpsilon) {
+    this.moveEpsilon = moveEpsilon;
+  }
+
+  public long getLongPressTimeout() {
+    return longPressTimeout;
+  }
+
+  public void setLongPressTimeout(long longPressTimeout) {
+    this.longPressTimeout = longPressTimeout;
+  }
 }
